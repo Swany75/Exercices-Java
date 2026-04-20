e516ae79eac1ca33912a05c77285615504118f86
Swany75 <95858269+Swany75@users.noreply.github.com>
2026-04-20 12:07:27 +0200

    Add TicTacToe UI, images & project configs
    Introduce a Swing-based TicTacToe game and related project files: replace TicTacToe main with a JPanel-based GUI (src/tictactoe/TicTacToe.java), add X/O images (src/images/X.png, O.png), and add nbproject/private/private.xml for the TicTacToe project. Update project settings: change javac.source/target to 25 for the TicTacToe project and add TicTacToe build/dist to .gitignore. Also update NetBeans user.properties.file paths in multiple nbproject/private/private.properties files to a Windows user path, and add an open-files entry in Exercici 08 private.xml.

--- Exercici 11 - TicTacToe/src/tictactoe/TicTacToe.java
+++ Exercici 11 - TicTacToe/src/tictactoe/TicTacToe.java
@@ -4,17 +4,159 @@
  */
 package tictactoe;
 
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
+
 /**
  *
- * @author swany
+ * @author Juan
  */
-public class TicTacToe {
 
-    /**
-     * @param args the command line arguments
-     */
+public class TicTacToe extends JPanel implements MouseListener, MouseMotionListener {
+
+    int rows = 3, cols = 3;
+    int cellWidth, cellHeight;
+
+    int pxWidth = 800;
+    int pxHeight = 800;
+
+    char[][] MATRIX = new char[rows][cols];
+    char turn;
+
+    boolean gameOver = false;
+
+    Image imgX;
+    Image imgO;
+
+    Random random = new Random();
+
+    Color chessDark = new Color(0x779556);
+    Color chessLight = new Color(0xEBECD0);
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        new TicTacToe().run();
     }
-    
+
+    private void run() {
+
+        imgX = new ImageIcon("src/images/X.png").getImage();
+        imgO = new ImageIcon("src/images/O.png").getImage();
+
+        // inicializar matriz (buena práctica)
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                MATRIX[i][j] = '\0';
+
+        turn = random.nextBoolean() ? 'x' : 'o';
+
+        JFrame frame = new JFrame("Tic Tac Toe");
+        frame.setIconImage(turn == 'x' ? imgX : imgO);
+
+        frame.setSize(pxWidth, pxHeight);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(this);
+        frame.setLocationRelativeTo(null);
+
+        this.addMouseListener(this);
+
+        frame.setVisible(true);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        cellWidth = getWidth() / cols;
+        cellHeight = getHeight() / rows;
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+
+                g.setColor((row + col) % 2 == 0 ? chessLight : chessDark);
+                g.fillRect(col * cellWidth, row * cellHeight, cellWidth, cellHeight);
+
+                if (MATRIX[row][col] == 'x') {
+                    g.drawImage(imgX, col * cellWidth, row * cellHeight, cellWidth, cellHeight, null);
+                } else if (MATRIX[row][col] == 'o') {
+                    g.drawImage(imgO, col * cellWidth, row * cellHeight, cellWidth, cellHeight, null);
+                }
+            }
+        }
+    }
+
+    private boolean checkWin(char p) {
+
+        boolean res;
+        
+        for (int i = 0; i < 3; i++) {
+            if (MATRIX[i][0] == p && MATRIX[i][1] == p && MATRIX[i][2] == p) { 
+                res = true;
+            }
+            if (MATRIX[0][i] == p && MATRIX[1][i] == p && MATRIX[2][i] == p) {
+                res = true;
+            }
+        }
+
+        if (MATRIX[0][0] == p && MATRIX[1][1] == p && MATRIX[2][2] == p) {
+            res = true;
+        }
+        if (MATRIX[0][2] == p && MATRIX[1][1] == p && MATRIX[2][0] == p) {
+            res = true;
+        }
+
+        return false;
+    }
+
+    private boolean isDraw() {
+        boolean res = true;
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (MATRIX[r][c] == '\0') {
+                    res = false;
+                }
+            }
+        }
+        return res;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+        if (gameOver) return;
+
+        int col = e.getX() / cellWidth;
+        int row = e.getY() / cellHeight;
+
+        if (row < 0 || row >= rows || col < 0 || col >= cols) return;
+        if (MATRIX[row][col] != '\0') return;
+
+        MATRIX[row][col] = turn;
+
+        if (checkWin(turn)) {
+            gameOver = true;
+            repaint();
+            JOptionPane.showMessageDialog(this, "Gana: " + turn);
+            return;
+        }
+
+        if (isDraw()) {
+            gameOver = true;
+            repaint();
+            JOptionPane.showMessageDialog(this, "Empate");
+            return;
+        }
+        
+        turn = (turn == 'x') ? 'o' : 'x';
+
+        repaint();
+    }
+
+    public void mousePressed(MouseEvent e) {}
+    public void mouseReleased(MouseEvent e) {}
+    public void mouseEntered(MouseEvent e) {}
+    public void mouseExited(MouseEvent e) {}
+    public void mouseDragged(MouseEvent e) {}
+    public void mouseMoved(MouseEvent e) {}
 }
